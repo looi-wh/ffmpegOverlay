789648676e61341a4c4de9b823d976f26ca0bab4
looi-wh <62279394+looi-wh@users.noreply.github.com>
2020-08-09 00:14:35 +0800

    Add files via upload
    added new end result page
    fixed a bug where ffmpeg refuses to create file if input and output filenames are the same
    corrected input-file codec detection method
    tested auto ignore function
    fine tuned some a few things

--- convert.py
+++ convert.py
@@ -10,12 +10,14 @@ clearScreenOption = 1
 # VIDEO SETTINGS:
 # note: read ffmpeg for this section
 arrayOfExtentions = [".avi", ".mkv", ".mov", ".mp4", ".wmv", ".flv", ".webm"] # please keep it to an array format
+#											^ note: if video is already processed/meets requirements, script will ignore
+#													"requirements" is video, audio and container matches requested
 containerFormat = ".mp4" # one container type only
 codecFormat = "h264" # video format (refrain from using copy)
 audioFormat = "aac" # audio format (refrain from using copy)
-preset = "veryfast"
-videoBitrate = "5M"
-audioBitrate = "192k"
+preset = "veryfast" # default: veryfast (a balance between quality and speed)
+videoBitrate = "5M" # default: 5M (can be viewed as mbps)
+audioBitrate = "320k" # default: 192k (normal quality, change to 320k for highest)
 # END OF SETTINGS
 
 # ADVANCED SETTINGS:
@@ -24,12 +26,14 @@ audioBitrate = "192k"
 cwd = os.getcwd() # current working directory, replace os.getcwd() to somewhere you would like
 removeNFO = 1 # removes the original nfo file
 removeOringalFile = 0 # removes the original media file
+showFiles = 1 # display the files before startings
 useFFMPEGBAR = 1 # uses ffmpeg-bar instead PLEASE HAVE THIS INSTALLED
 illegalChar = ["'", '"'] # characters to remove from file name
-extraCommands = "-map 0:0 -map 0:a -map '0:s?' -crf 20 -c:s mov_text " # important stuff
+extraCommands = "-map 0:0 -map 0:a -map '0:s?' -crf 20 -c:s mov_text" # important stuff
 # 				^ maps video, audio(all) and subtitles(all - if it exists). quality = 20 (higher than average). subititle = mov_test
 
 
+# functions (some i created a long time ago)
 def clearScreen(): # for screen clearing. can be disabled using clearScreenOption
 	# version 3: optimized with better automation
 	global clearScreenOption
@@ -62,13 +66,18 @@ def renameFiles(arrayOfTargets):
 		for y in targetFiles:
 			os.rename(y, y.replace(x, ""))
 
+def renameSpecific(file, target, replace):
+	os.rename(file, file.replace(target, replace))
+
 def combineArray(input):
 	combine = []
 	for x in input:
 		for y in x:
 			combine.append(y)
 	return combine
+# end of functions
 
+# start of main
 clearScreen()
 # declare to user
 print("[SETTINGS] looking at", str(cwd))
@@ -79,66 +88,98 @@ print("[SETTINGS] containerFormat:", containerFormat)
 print("[SETTINGS] codecFormat:", codecFormat)
 print("[SETTINGS] audioFormat:", audioFormat)
 print("[SETTINGS] preset:", preset)
-print("[SETTINGS] videoBitrate:", audioBitrate)
-print("[SETTINGS] audioBitrate:", videoBitrate)
-# find files
+print("[SETTINGS] videoBitrate:", videoBitrate)
+print("[SETTINGS] audioBitrate:", audioBitrate)
+# prepare variable
 fileCount = 0
 # rename files
-renameFiles(illegalChar)
-targetFiles = filterContent(arrayOfExtentions, cwd)
-for x in targetFiles:
-	print("[FILES] found", x)
-	fileCount += 1
+renameFiles(illegalChar) # removes illegal characters that might screw with the script
+targetFiles = filterContent(arrayOfExtentions, cwd) # checks recursively inside folders
+for x in targetFiles: # for loop files
+	if showFiles == 1: # respect settings
+		print("[FILES] found", x) # show files and location (good for double checking)
+	fileCount += 1 # counter
 print("[TOTAL] number of target(s) found:", fileCount)
-input("[READY] Press Enter to accept and start job...")
+if fileCount == 0:
+	print("[WARNING] try checking file premission and the working directory in the script")
+	print("[WARNING] maybe format detect doesnt include the extension you want")
+else:
+	input("[READY] Press Enter to accept and start job...") # wait for user permission to start. crtl + c will not do anything at this point
 doneCount = 0
 # start converting
 for target in targetFiles:
-	clearScreen()
+	targetZero = target
 	tempcodecFormat = codecFormat
 	tempaudioFormat = audioFormat
 	for codecs in arrayOfExtentions:
 		if not target.count(codecs) == 0:
-			# check video codec:
+			if str(containerFormat) in str(target):
+				renameSpecific(target, codecs, str(".input") + str(codecs))
+				target = target.replace(codecs, str(".input") + str(codecs))
+			# prepare commands that to be used later
 			commandCheckVideo = "ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 '" + target +"'"
 			commandCheckAudio = "ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 '" + target +"'"
 			commandCheckSub = "ffmpeg -i '" + target + "' -c copy -map 0:s -f null - -v 0 -hide_banner && echo $? || echo $?"
 			commandCheckStreams = "ffprobe '" + target + "' -show_entries format=nb_streams -v 0 -of compact=p=0:nk=1"
 			skip = 0
-			if subprocess.check_output(commandCheckVideo, shell=True) == codecFormat:
+			if codecFormat in str(subprocess.check_output(commandCheckVideo, shell=True)): # copy if matches for faster transcoding
 				skip += 1
 				tempcodecFormat = "copy"
-			if subprocess.check_output(commandCheckAudio, shell=True) == audioFormat:
+			if audioFormat in str(subprocess.check_output(commandCheckAudio, shell=True)): # copy if matches for faster transcoding
 				skip += 1
 				tempaudioFormat = "copy"
-			if not skip == 2: #skip if all four passes
-				output =  target.replace(codecs, containerFormat)
-				deleteNFO = target.replace(codecs, ".nfo")
-				if useFFMPEGBAR == 1:
-					print("[CONVERTING] working on", target, "using ffmpeg-bar")
-					print("[CONVERTING] output to", output)
-					print("[CONVERTING] file", doneCount+1, "of", fileCount)
+			if str(containerFormat) in str(target): # if video and audio matches except for container, script will only ask for remux
+				skip += 1
+			if not skip == 3: #skip if all four passes
+				output =  targetZero.replace(codecs, containerFormat) # output filename and container
+				deleteNFO = targetZero.replace(codecs, ".nfo") # jellyfin nfo reset (just in case the file was renamed due to illegal char. its easier to ask jellyfin to process again)
+				print("[CONVERTING] working on", targetZero)
+				print("[CONVERTING] output to", output)
+				print("[CONVERTING] processing file", doneCount+1, "of", fileCount)
+				print("[DETAILS] original video codec:", subprocess.check_output(commandCheckVideo, shell=True))
+				print("[DETAILS] original audio codec:", subprocess.check_output(commandCheckAudio, shell=True))
+				if useFFMPEGBAR == 1: # recommended cause of ETA and progress bar
+					print("[CONVERTING] using ffmpeg-bar")
 					command = "ffmpeg-bar -i '" + target + "' " + "-c:v " + tempcodecFormat + " " + extraCommands + " -c:a " + tempaudioFormat + " -preset " + preset +" -y -b:v " + videoBitrate +" -b:a " + audioBitrate + " '" + output + "'"
 				else:
-					print("[CONVERTING] working on", target, "using ffmpeg only")
-					print("[CONVERTING] output to", output)
-					print("[CONVERTING] file", doneCount+1, "of", fileCount)
+					print("[CONVERTING] using ffmpeg only")
 					print("[CONVERTING] please wait, script might look non-responsive for large file")
 					command = "ffmpeg -i '" + target + "' " + "-c:v " + tempcodecFormat + " " + extraCommands + " -c:a " + tempaudioFormat + " -preset " + preset +" -y -b:v " + videoBitrate +" -b:a " + audioBitrate + " '" + output + "' 2> /dev/null"
 				os.system(command) # run command into system
-				if os.path.exists(output):
+				if os.path.exists(output): # checks for output file first
 					if str(containerFormat) in str(target): 
 						print("ignoring delete for", output) # if output format = input format, do not delete output file
 					else:
-						if removeOringalFile == 1:
+						if removeOringalFile == 1: # respect user settings
 							if os.path.exists(target):
 								os.remove(target) # removes original file
 						if os.path.exists(deleteNFO):
 							if removeNFO == 1:
 								os.remove(deleteNFO) # deletes the nfo file
+			else:
+				print("[NOTICE] ignored file", doneCount + 1, "of", fileCount,"as requirements has already been met")
+				renameSpecific(target, ".input", "")
+
 	doneCount += 1
 
+print("")
+print("END RESULT:")
 targetFiles = filterContent(arrayOfExtentions, cwd)
-clearScreen()
+successCount = 0
+failCount = 0
 for x in targetFiles:
-	print("[finished] found", x)
+	commandCheckVideo = "ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 '" + x +"'"
+	commandCheckAudio = "ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 '" + x +"'"
+	commandCheckSub = "ffmpeg -i '" + x + "' -c copy -map 0:s -f null - -v 0 -hide_banner && echo $? || echo $?"
+	if codecFormat in str(subprocess.check_output(commandCheckVideo, shell=True)) and audioFormat in str(subprocess.check_output(commandCheckAudio, shell=True)) and str(containerFormat) in str(x):
+		print("[SUCCESS] verified", x)
+		successCount += 1
+	else:
+		print("[FAILED] did not passed", x)
+		failCount += 1
+if not fileCount == 0:
+	print("[RESULT]", successCount, "file(s) verified")
+	print("[RESULT]", failCount, "file(s) failed")
+	print("[RESULT]", doneCount, "file(s) processed")
+print("[END] job done")
+
